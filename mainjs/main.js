715b73a6aa3bcc5d598d68e52e978882d35a2d56
pingandthepong <pingandthepong@gmail.com>
2024-03-26 14:56:26 +0900

    jquery 파일 연결 및 sub4 초기세팅

---
+++ mainjs/main.js
@@ -0,0 +1,19 @@
+// .master           
+// $('.master_tab_list li:eq(0)').fadeIn('slow');
+// $('.master_tab li:eq(0) a').css({
+//   "color": "white",
+//   "backgroundColor": "#ef0010",
+// });
+   
+// $('.master_tab a').click(function(e){
+//   e.preventDefault();
+  
+//   const ind = $(this).index('.master_tab a'); // 0 1 2 3
+//   console.log(ind);
+  
+//   $('.master_tab_list li').hide();
+//   $('.master_tab_list li:eq('+ind+')').fadeIn('slow');
+
+//   $('.master_tab a').css('color','#333').css('background','#fff');
+//   $(this).css('color','red').css('background','#0f0');
+// });
