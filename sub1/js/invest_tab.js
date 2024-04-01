21e613d3fd98cbc97f859aa82a0d2c7e9a981c36
pingandthepong <pingandthepong@gmail.com>
2024-04-01 20:59:57 +0900

    sub1 파비콘 추가 및 sub1_3 탭메뉴 기능 추가

---
+++ sub1/js/invest_tab.js
@@ -0,0 +1,13 @@
+$('.tab_com:first').show();
+
+$('.sub_list a').click(function (e) {
+  e.preventDefault();
+  var tabInd = $(this).index('.sub_list a');
+  
+  $(this).parents('.sub_list').find('li').removeClass('current');
+  $(this).parent().addClass('current');
+
+  $('.tab_com').hide();
+  $(`.tab${tabInd + 1}`).show();
+  
+})
