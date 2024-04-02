2f86e3b90f1456cf903c14597351a2980b69f7cd
pingandthepong <pingandthepong@gmail.com>
2024-04-03 04:42:18 +0900

    footer family_site 클릭 시 바디를 클릭해도 닫히도록 기능 추가

--- common/js/fullnav.js
+++ common/js/fullnav.js
@@ -1,4 +1,3 @@
-
 var smh = $(".visual").height();
 var on_off = false; // true(오버) false(오버X)
 
@@ -42,11 +41,44 @@ $("#headerArea").mouseleave(function () {
   on_off = false;
 });
 
-$(window).on("scroll", function () {
-  var scroll = $(window).scrollTop();
+// $(window).scroll(function(event) {
+//   var scroll = $(window).scrollTop();
 
-  if (scroll > smh - 105) {
-    $("#headerArea").css({
+//   if (scroll > smh - 105) {
+//     $("#headerArea").css({
+//       "background": "#fff",
+//       "boxShadow":
+//         "0 0 5px 0 rgba(0, 0, 0, 0.5), 2px 2px 10px 0 rgba(0, 0, 0, 0.1)",
+//     });
+//     $("#headerArea .logo a").css({
+//       "background": "url('https://pingandthepong.mycafe24.com/common/images/KUMHO_Logo_KJH_txtblack_pb20.png') no-repeat 0 0",
+//     });
+//     $(".depth1, .signin a").css({ "color": "#111" });
+//   } else {
+//     if (on_off == false) {
+//       $("#headerArea").css({
+//         "background": "transparent",
+//         "boxShadow": "none",
+//       });
+//       $("#headerArea .logo a").css({
+//         "background": "url('https://pingandthepong.mycafe24.com/common/images/header-logo-pb20.png') no-repeat 0 0",
+//       });
+//       $(".depth1, .signin a").css({ "color": "#fff" });
+//     }
+//   }
+// });
+
+// 스크롤에 따라 헤더 숨기고, 보이기
+var lastScrollTop = 0;
+var delta = 100;
+var headerHeight = $('#headerArea').outerHeight();
+
+$(window).scroll(function(event) {
+  var st = $(this).scrollTop();
+
+  // 스크롤 방향 체크
+  if (Math.abs(lastScrollTop - st) <= delta) {
+        $("#headerArea").css({
       "background": "#fff",
       "boxShadow":
         "0 0 5px 0 rgba(0, 0, 0, 0.5), 2px 2px 10px 0 rgba(0, 0, 0, 0.1)",
@@ -55,18 +87,21 @@ $(window).on("scroll", function () {
       "background": "url('https://pingandthepong.mycafe24.com/common/images/KUMHO_Logo_KJH_txtblack_pb20.png') no-repeat 0 0",
     });
     $(".depth1, .signin a").css({ "color": "#111" });
-  } else {
-    if (on_off == false) {
-      $("#headerArea").css({
-        "background": "transparent",
-        "boxShadow": "none",
-      });
-      $("#headerArea .logo a").css({
-        "background": "url('https://pingandthepong.mycafe24.com/common/images/header-logo-pb20.png') no-repeat 0 0",
-      });
-      $(".depth1, .signin a").css({ "color": "#fff" });
-    }
+
+    return;
   }
+
+  if (st > lastScrollTop && st > headerHeight){
+    // 아래로 스크롤 시 헤더 숨기기
+    // $('#headerArea').css('top', '-' + headerHeight + 'px');
+    $('#headerArea').slideUp(300);
+  } else {
+    // 위로 스크롤 시 헤더 다시 표시
+    // $('#headerArea').css('top', 0);
+    $('#headerArea').slideDown(300);
+  }
+  
+  lastScrollTop = st;
 });
 
 //2depth 열기/닫기
@@ -185,15 +220,33 @@ $(".top_menu a").focus(function () {
 
 
 // footer family site
-$('.family_site .arrow').toggle(
-  function () {
+$('.family_site .arrow').click(function (e) {
+  e.preventDefault();
+
+  var famOnOff = $(this).data('famOnOff');
+
+  if (!famOnOff) {
     $('.family_site ul').stop().slideDown();
-    $(this).find('i').delay(150).animate({rotate: '-180deg'}, 1);
-  },
-  function () {
+    $(this).find('i').animate({rotate: '-180deg'}, 100);
+    $(this).data('famOnOff', true);
+  } else {
     $('.family_site ul').stop().slideUp('fast');
     $(this).find('i').animate({ rotate: '0deg' }, 100);
-  });
+    $(this).data('famOnOff', false);
+  }
+});
+
+// family_site가 열려있을 때, body 클릭 시 닫히게 하기
+$('body').click(function(event) {
+  famOnOff = $('.family_site .arrow').data('famOnOff');
+  if (famOnOff) {
+    if (!$(event.target).closest('.family_site').length) {
+      $('.family_site ul').stop().slideUp('fast');
+      $('.family_site .arrow').find('i').animate({ rotate: '0deg' }, 100);
+      $('.family_site .arrow').data('famOnOff', false);
+    }
+  }
+});
 
 // footer family site - tab 처리
 $('.family_site .arrow').focus(function () {$('.family_site ul').slideDown();});
@@ -204,7 +257,7 @@ $('.family_site ul li:last a').blur(function () {$('.family_site ul').slideUp('f
 $(window).on('scroll',function(){
   var scroll = $(window).scrollTop();
   
-  if (scroll > 900) {
+  if (scroll > 300) {
     $('.move_top').css('opacity', '1');
   } else {
     $('.move_top').css('opacity','0');
@@ -215,4 +268,5 @@ $('.move_top').click(function(e){
   e.preventDefault();
   $("html,body").stop().animate({"scrollTop":0}, 1000);
 });
+
 
