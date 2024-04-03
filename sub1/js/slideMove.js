b7bcaa4509a9928829ab3ed3873e22c883c567a7
pingandthepong <pingandthepong@gmail.com>
2024-04-03 23:05:04 +0900

    sub1_2 연력 스크롤 작업 기본 기능 추가

--- sub1/js/slideMove.js
+++ sub1/js/slideMove.js
@@ -13,38 +13,13 @@ $('.slideMenu a').click(function(e) {
 
   if ($(this).hasClass(`link${hisInd + 1}`)) {
 
-    value = $(`.con${hisInd + 1}`).offset().top;
+    // #headerArea 기능 추가 전까지 빼기 값 추가
+    value = $(`.con${hisInd + 1}`).offset().top - $('#headerArea').outerHeight() - 20;
   }
 
   $('html,body').stop().animate({scrollTop: value}, 500);
 
 });
 
-// 1417 => 1524 (107 의 차이는 어디에서 나는ㄴ걸까? #headerArea ??)
 
 // 스크롤이 .slideMenu를 넘어서면 .slideMenu의 위치 변경하기
-// var smh = $('.intro__title').height();
-// console.log(`.height() = ${smh}`);
-
-
-$(window).on('scroll',function() {
-  var scroll = Math.ceil($(window).scrollTop());
-  console.log(scroll);
-
-  if (scroll > 1300) {
-    $('.slideMenu')
-      .css({
-        position: 'fixed',
-        left: '50%',
-        // bottom: 0,
-        transform: 'translateX(-50%)',
-        marginBottom: 0,
-      })
-    .animate({
-      bottom: 0,
-    })
-    
-  } else {
-    // $('.slideMenu').animate({bottom: 0,});
-  }
-})
