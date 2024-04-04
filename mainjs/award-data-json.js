8d66ffb141543009d1ce148d6ea0c3c423b78ee0
pingandthepong <pingandthepong@gmail.com>
2024-04-05 01:18:45 +0900

    index award .more_btn perspective 추가 & json과의 충돌로 임시로 awardSlide.js 주석 처리

--- mainjs/award-data-json.js
+++ mainjs/award-data-json.js
@@ -1,3 +1,5 @@
+// $(document).ready(function() {
+
 // XMLHttpRequest 객체 생성
 var xhr = new XMLHttpRequest();                 
 
@@ -5,7 +7,7 @@ var xhr = new XMLHttpRequest();
 xhr.onload = function() {                       
  
   // If server status was ok
-  //if(xhr.status === 200) {                      
+  if(xhr.status === 200) {                      
     
   //서버로 부터 전달된 json 데이터(단순 텍스트)를 responseText 속성을 통해 가져올 수 있다.
   // ⭐️ parse() 메소드를 호출하여 자바스크립트 객체로 변환한다.
@@ -13,43 +15,39 @@ xhr.onload = function() {
     responseObject = JSON.parse(xhr.responseText);
 
 
-    // console.log(responseObject.events[1].map);
-    // console.log(responseObject.events[2].location);
-    
+    // console에는 잘 찍히는데, 화면에 출력이 안됨
     // console.log(responseObject.li[0].href);
     // console.log(responseObject.li[0].imgsrc);
     // console.log(responseObject.li[0].title);
     // console.log(responseObject.li[0].institution);
     // console.log(responseObject.li[0].date);
 
-
     var newContent = '';
-    for (var i = 0; i < responseObject.li.length; i++) { 
+    
+
+    responseObject.li.forEach(function(item) {
       newContent += '<li>';
-      // newContent += '<img src="' + responseObject.events[i].map + '" ';
-      // newContent += 'alt="' + responseObject.events[i].href + '" />';
-      // newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
-      // newContent += responseObject.events[i].date + '</p>';
-      newContent += `<a href="${responseObject.li[i].href}">`;
-      newContent += `<div class="img_wrap"><img src="${responseObject.li[i].imgsrc}" alt=""></div>`;
+      newContent += `<a href="${item.href}">`;
+      newContent += `<div class="img_wrap"><img src="${item.imgsrc}" alt=""></div>`;
       newContent += `<dl class="award_txt">`;
-      newContent += `<dt>${responseObject.li[i].title}</dt>`;
-      newContent += `<dd class="award_institution">${responseObject.li[i].institution}</dd>`;
-      newContent += `<dd class="date">${responseObject.li[i].date}</dd>`;
+      newContent += `<dt>${item.title}</dt>`;
+      newContent += `<dd class="award_institution">${item.institution}</dd>`;
+      newContent += `<dd class="date">${item.date}</dd>`;
       newContent += `</dl>`;
       newContent += `</a>`;
       newContent += '</li>';
-    }
-
+    })
  
     document.getElementById('award-data').innerHTML = newContent;
 
-  //}
+  } else {
+    console.error('Error loading data. Status code: ' + xhr.status);
+  }
 };
 
-// 요청을 준비한다.
-// xhr.open('GET', 'data/data.json', true);        
-xhr.open('GET', './data/award.json', true);        
-
-// 요청을 전송한다
+// 요청 준비 및 전송
+xhr.open('GET', './data/award.json', true);
 xhr.send(null);
+
+
+// });
