533986b7e72afb89b3facd8d1889591732e0f6b4
pingandthepong <pingandthepong@gmail.com>
2024-04-04 23:47:44 +0900

    index award 좌우버튼 없애고, 자동 슬라이드 기능만 하기로 결정함(박스너비 등의 문제)

--- mainjs/awardSlide.js
+++ mainjs/awardSlide.js
@@ -14,6 +14,11 @@ const listLength = $('.award_list li').length; // 6
 // li 개당 너비(margin 포함)
 const listOuterWidth = $('.award_list li').outerWidth(true); // 537
 
+// 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
+const range = ulWidth + (ulWidth - slideContainer); // 4524
+// 전체를 감싸는 고정된 .award__container에서 빠져나간 부분
+const remaining = ulWidth - slideContainer; // 1302
+
 
 
 $('.award_list ul').after($('.award_list ul').clone());
@@ -26,8 +31,8 @@ function awardMove () {
   $('.award_list').css({left: position});
   
   
-  // 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
-  const range = ulWidth + (ulWidth - slideContainer); // 4524
+  // // 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
+  // const range = ulWidth + (ulWidth - slideContainer); // 4524
   
   if(position < -range ) {
     $('.award_list').css({ left : slideContainer - ulWidth});
@@ -35,19 +40,52 @@ function awardMove () {
   }
 }
 
-timeonoff = setInterval(awardMove, 100);
+// timeonoff = setInterval(awardMove, 100);
 
 
 // .award_list & 좌우버튼 호버시 자동 슬라이드 멈춤
-$('.award_list, .btn_box a:not(:last)').hover(
-  function() {
-    clearInterval(timeonoff);
+// $('.award_list, .btn_box a:not(:last)').hover(
+//   function() {
+//     clearInterval(timeonoff);
     
-  },
-  function() {
-    timeonoff = setInterval(awardMove, 100);
+//   },
+//   function() {
+//     timeonoff = setInterval(awardMove, 100);
     
-  }
-);
+//   }
+// );
 
+
+// 위의 자동 슬라이드 기능은 사용하지 않을 예정
 // 좌우버튼 클릭시 넘기기
+$('.btn').click(function(e) {
+  e.preventDefault();
+
+  
+  var direction = $(this).hasClass('leftBtn') ? 'left' : 'right';
+  moveSlide(direction);
+})
+
+
+function moveSlide(direction) {
+
+  if (direction == 'left') {
+
+    position -= listOuterWidth;
+    
+    $('.award_list').animate({left: position}, 'fast', function() {
+
+      if (position <= -range ) {
+        position = -remaining;
+        $('.award_list').css({left: -remaining});        
+      }
+    }).clearQueue();
+
+  } else if (direction == 'right') {
+    position += listOuterWidth;
+
+    $('.award_list').animate({left: position}, 'fast', function() {
+      
+    }).clearQueue();
+  }
+}
