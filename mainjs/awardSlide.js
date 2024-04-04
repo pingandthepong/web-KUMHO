1fdf0b6937b572c510d54110191ed100bce0930d
pingandthepong <pingandthepong@gmail.com>
2024-04-04 17:15:15 +0900

    index award 자동 슬라이드 성공

--- mainjs/awardSlide.js
+++ mainjs/awardSlide.js
@@ -1,23 +1,37 @@
-const movesize = 2;
+const movesize = 1.5;
 let position = 0;
 let timeonoff;
 
+// 전체를 감싸는 고정된 .award__container 너비
+const slideContainer = $('.award__container').width(); // 1920
 
-const ulWidth = $('.award_list').width(); // 2148
-const listLength = $('.award_list li').length; // 4
+// ul 한 덩이의 너비
+const ulWidth = $('.award_list ul').width(); // 3222
+
+// li 개수
+const listLength = $('.award_list li').length; // 6
+
+// li 개당 너비(margin 포함)
 const listOuterWidth = $('.award_list li').outerWidth(true); // 537
 
-$('.award_list ul').after($('.award_list ul').clone()); // 4296
 
-// 자동 슬라이드
+
+$('.award_list ul').after($('.award_list ul').clone());
+
+
+// 자동 슬라이드 함수
 function awardMove () {
   position -= movesize;
   $('.award_list').css({left: position});
-  // if(position >= ) {
-
-  // }
-
-
+  
+  
+  // 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
+  const range = ulWidth + (ulWidth - slideContainer); // 4524
+  
+  if(position < -range ) {
+    $('.award_list').css({ left : 'slideContainer - ulWidth'});
+    position = slideContainer - ulWidth;
+  }
 }
 
 timeonoff = setInterval(awardMove,100);
@@ -25,13 +39,13 @@ timeonoff = setInterval(awardMove,100);
 
 
 // .award_
-$('.award_list li').hover(
-  function() {
-    clearInterval(awardMove);
+// $('.award_list').hover(
+//   function() {
+//     clearInterval(awardMove);
     
-  },
-  function() {
-    timeonoff = setInterval(awardMove, 100);
+//   },
+//   function() {
+//     timeonoff = setInterval(awardMove, 100);
     
-  }
-);
+//   }
+// );
