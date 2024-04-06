99bc1c6a18254c64f6e41d4075fd77e800cc2ed9
pingandthepong <pingandthepong@gmail.com>
2024-04-06 17:13:36 +0900

    sub2_5 jquery ajax로 검색 기능 추가

--- sub2/js/performance.js
+++ sub2/js/performance.js
@@ -18,7 +18,7 @@ $(function() {
     success: function(data) {
       // 전체 배열 안 객체 저장
       var useData = data.performance;
-      // console.log(useData); // Array(12)
+      // console.log(useData); // Array(15)
 
       // 검색 기능을 위한 매개변수 arr
       function dataPrint(arr) {
@@ -44,17 +44,42 @@ $(function() {
         $('.performance_list').html(txt);
       }
 
+      // 초기실행 함수 호출
       dataPrint(useData);
       
 
+      // 배경 사진
       $('.performance_list ul li').each(function(idx, list) {
-        console.log(idx); // 0 ~ 11
+        // console.log(idx); // 0 ~ 14
 
         $(this).find($('.all_img_container')).css({
           background: `url(${useData[idx].bgUrl}) no-repeat center`,
         });
       });
-    }
-  })
 
+
+      // 검색 버튼 클릭 시
+      $('#searchBtn').click(function() {
+        var value = $('#performance').val();
+
+        var newArray = useData.filter(function(element) {
+          return element.state01.includes(value) || element.state02.includes(value) || element.name.includes(value);
+        });
+
+        // 없는 값 검색 시 에러 문구
+        if(newArray.length === 0) {
+          $('.performance_list').html('<p>검색 결과가 없습니다.</p>');
+        } else {
+          dataPrint(newArray);
+        }
+      });
+      
+      // 검색어 입력 후 엔터를 누르면 검색 버튼이 클릭되도록
+      $('#performance').keypress(function(e) {
+        if (e.keyCode === 13) {
+          $('#searchBtn').click();
+        }
+      })
+    }
+  });
 });
