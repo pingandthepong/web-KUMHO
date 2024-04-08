324d994589dfe38719be3e26348d84c4173901d8
pingandthepong <pingandthepong@gmail.com>
2024-04-09 00:23:58 +0900

    sub1_3 도넛그래프 완성

--- sub1/js/invest3Graph.js
+++ sub1/js/invest3Graph.js
@@ -1,5 +1,5 @@
 const data = {
-  // labels: ['최대주주 및 그 특수 관계인', '국내기관', '자사주', '외국인', '국내개인 및 기타'],
+  labels: ['최대주주 및 그 특수 관계인', '국내기관', '자사주', '외국인', '국내개인 및 기타'],
   datasets: [{
     label: '보통주',
     data: [47.51, 13, 2.86, 3.96, 1],
@@ -10,18 +10,35 @@ const data = {
       '#0097D7',
       '#00D0FF'
     ],
-    hoverOffset: 10
+    hoverOffset: 10,
   }]
 };
             
 const config = {
   type: 'doughnut',
   data: data,
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+        onClick: function(event, legendItem) {
+          // 클릭 이벤트 무시
+          event.stopPropagation();
+        }
+      },
+      tooltip: {
+        bodyFont: {
+          size: 16
+        },
+        titleFont: {
+          size: 18
+        }
+      }
+    }
+  }
 };
 
 const invest3Graph = new Chart(
       document.getElementById('invest3Graph'),
       config
 );
-
-
