08a3f14ae64220c933fcf49f16b90f8ac419baf3
pingandthepong <pingandthepong@gmail.com>
2024-04-09 02:33:19 +0900

    index award .btn_box 기능 완료

--- mainjs/awardSlide.js
+++ mainjs/awardSlide.js
@@ -1,46 +1,54 @@
 export function awardSlide() {
+  // li 개당 너비(margin 포함)
+  const listOuterWidth = $(".award_list li").outerWidth(true); // 520
 
-// 전체를 감싸는 고정된 .award__container 너비
-const slideContainer = $('.award__container').width();
+  // 전체를 감싸는 고정된 .award__container에서 빠져나간 부분
+  const remaining = listOuterWidth * 5;
 
-// ul 한 덩이의 너비
-const ulWidth = $('.award_list ul').width();
+  const movesize = listOuterWidth;
+  let position = 0;
 
+  // leftBtn
+  function awardMoveL() {
+    position -= movesize;
 
-// li 개수
-const listLength = $('.award_list li').length; // 8
+    $(".award_list").stop().animate({ left: position }, { duration: 200, easing: "easeInOutQuad" });
 
-// li 개당 너비(margin 포함)
-const listOuterWidth = $('.award_list li').outerWidth(true); // 520
-
-// 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
-const range = ulWidth - slideContainer; // 2080
-
-// 전체를 감싸는 고정된 .award__container에서 빠져나간 부분
-const remaining = listOuterWidth * 5
-
-
-
-
-const movesize = listOuterWidth;
-let position = 0;
-
-
-function awardMove () {
-  position -= movesize;
-
-  $('.award_list').animate({left: position}, {duration: 200, easing: 'easeInOutQuad'});
-  
-  if(position <= -remaining ) {
-    $('.btn_box .leftBtn').addClass('disabled');
+    if (position <= -remaining) {
+      $(".btn_box .leftBtn").addClass("disabled");
+    } else {
+      $(".btn_box .leftBtn").removeClass("disabled");
+    }
   }
-}
 
+  // rightBtn
+  function awardMoveR() {
+    position += movesize;
+    $(".award_list").stop().animate({ left: position }, { duration: 200, easing: "easeInOutQuad" });
 
-$('.award_list, .btn_box .btn').on('click', function(e) {
-  e.preventDefault();
-  awardMove();
+    if (position >= 0) {
+      $(".btn_box .rightBtn").addClass("disabled");
+    } else {
+      $(".btn_box .rightBtn").removeClass("disabled");
+    }
+  }
+
   
-});
+  // 최초 우측 버튼 비활성화
+  $(document).ready(function () {
+    $(".btn_box .rightBtn").addClass("disabled");
+  });
 
+
+  $(".btn_box .btn").on("click", function (e) {
+    e.preventDefault();
+
+    if ($(this).hasClass("leftBtn")) {
+      awardMoveL();
+      $(".btn_box .rightBtn").removeClass("disabled");
+    } else if ($(this).hasClass("rightBtn")) {
+      awardMoveR();
+      $(".btn_box .leftBtn").removeClass("disabled");
+    }
+  });
 }
