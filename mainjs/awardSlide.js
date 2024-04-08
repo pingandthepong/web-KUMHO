6c5cd2a5eef87d8a8f6cc700aee8b02aef3ac3b8
pingandthepong <pingandthepong@gmail.com>
2024-04-09 02:12:07 +0900

    index award .leftBtn .disabled 추가

--- mainjs/awardSlide.js
+++ mainjs/awardSlide.js
@@ -1,21 +1,25 @@
 export function awardSlide() {
 
 // 전체를 감싸는 고정된 .award__container 너비
-const slideContainer = $('.award__container').width(); // 2080
+const slideContainer = $('.award__container').width();
 
 // ul 한 덩이의 너비
-const ulWidth = $('.award_list ul').width(); //2600
+const ulWidth = $('.award_list ul').width();
+
 
 // li 개수
-const listLength = $('.award_list li').length; // 5
+const listLength = $('.award_list li').length; // 8
 
 // li 개당 너비(margin 포함)
 const listOuterWidth = $('.award_list li').outerWidth(true); // 520
 
 // 전체를 감싸는 고정된 .award__container에서 빠져나간 .award_list를 더한 좌표값
-const range = ulWidth + (ulWidth - slideContainer); // 4524
+const range = ulWidth - slideContainer; // 2080
+
 // 전체를 감싸는 고정된 .award__container에서 빠져나간 부분
-const remaining = ulWidth - slideContainer; // 1302
+const remaining = listOuterWidth * 5
+
+
 
 
 const movesize = listOuterWidth;
@@ -25,17 +29,17 @@ let position = 0;
 function awardMove () {
   position -= movesize;
 
-  $('.award_list').css({left: position});
+  $('.award_list').animate({left: position}, {duration: 200, easing: 'easeInOutQuad'});
   
-  if(position < -range ) {
-    $('.award_list').css({ left : slideContainer - ulWidth});
-    position = slideContainer - ulWidth;
+  if(position <= -remaining ) {
+    $('.btn_box .leftBtn').addClass('disabled');
   }
 }
 
 
 $('.award_list, .btn_box .btn').on('click', function(e) {
   e.preventDefault();
+  awardMove();
   
 });
 
