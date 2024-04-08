61f159a08574f8bfbfe7711b9ff2c50e07f1287b
pingandthepong <pingandthepong@gmail.com>
2024-04-08 23:01:15 +0900

    sub1_3 투자정보 - 재무상태표 완료

--- sub1/js/fPosition.js
+++ sub1/js/fPosition.js
@@ -2,16 +2,6 @@ Highcharts.chart('fPosition', {
   chart: {
       type: 'column'
   },
-  // 범례 설정
-  legend: {
-    align: 'right',
-    verticalAlign: 'top',
-    layout: 'horizontal',
-    x: 0,
-    y: -20,
-    
-
-  },
   title: {
       text: '',
       align: 'center'
@@ -21,21 +11,76 @@ Highcharts.chart('fPosition', {
       crosshair: true,
       accessibility: {
           description: 'Years'
-      }
+      },
+      labels: {
+        style: {
+            fontSize: '19px',
+            color: '#666',
+        }
+    }
   },
   yAxis: {
       min: 0,
       title: {
           text: ''
-      }
+      },
+      labels: {
+        format: '{value:,.0f}',
+        style: {
+            fontSize: '15px',
+        }
+    }
+  },
+  credits: {
+    enabled: false
   },
   plotOptions: {
       column: {
+          series: {
+            borderWidth: 0,
+            groupPadding: 0.1,
+          },
           pointPadding: 0.2,
-          borderWidth: 0,
-         
-          
-      }
+          dataLabels: {
+            enabled: true,
+            inside: false, // 레이블을 열 안쪽에 표시
+            color: '#333', // 레이블 색상 설정
+            style: {
+                fontSize: '15px',
+                fontWeight: '600',
+                textOutline: 'none' // 텍스트 외곽선 제거
+            },
+            formatter: function() {
+              return Highcharts.numberFormat(this.y, 0, '', ','); // 데이터 값 포맷팅
+          }
+        }
+      },
+  },
+  tooltip: {
+    style: {
+      fontSize: '19px',
+      padding: '10px'
+    }
+  },
+  // 범례 설정
+  legend: {
+    align: 'right',
+    verticalAlign: 'top',
+    layout: 'horizontal',
+    x: 0,
+    y: 0,
+    symbolRadius: 1,
+    symbolWidth: 24,
+    symbolHeight: 24,
+    symbol: function() {
+      return 'rect';
+    },
+    itemStyle: {
+      fontSize: '19px'
+    },
+    itemMarginTop: 0,
+    itemMarginBottom: 50,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white'
   },
   series: [
       {
@@ -55,5 +100,3 @@ Highcharts.chart('fPosition', {
       }
   ]
 });
-
-document.getElementById('legend-container').innerHTML = document.getElementById('container').innerHTML;
