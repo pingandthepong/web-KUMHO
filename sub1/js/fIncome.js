5003e4e7b94830e78927ce43bb1cabb33622cbd0
pingandthepong <pingandthepong@gmail.com>
2024-04-08 23:43:01 +0900

    sub1_3 파일 format on save 적용

--- sub1/js/fIncome.js
+++ sub1/js/fIncome.js
@@ -1,112 +1,112 @@
-Highcharts.chart('fIncome', {
+Highcharts.chart("fIncome", {
   chart: {
-      type: 'column'
+    type: "column",
   },
   title: {
-      text: '',
-      align: 'center'
+    text: "",
+    align: "center",
   },
   xAxis: {
-      categories: ['2021년', '2022년', '2023년'],
-      crosshair: true,
-      accessibility: {
-          description: 'Years'
+    categories: ["2021년", "2022년", "2023년"],
+    crosshair: true,
+    accessibility: {
+      description: "Years",
+    },
+    labels: {
+      style: {
+        fontSize: "19px",
+        color: "#666",
       },
-      labels: {
-        style: {
-            fontSize: '19px',
-            color: '#666',
-        }
-    }
+    },
   },
   yAxis: {
-      min: 0,
-      title: {
-          text: ''
+    min: 0,
+    title: {
+      text: "",
+    },
+    labels: {
+      format: "{value:,.0f}",
+      style: {
+        fontSize: "15px",
       },
-      labels: {
-        format: '{value:,.0f}',
-        style: {
-            fontSize: '15px',
-        }
-    }
+    },
   },
   credits: {
-    enabled: false
+    enabled: false,
   },
   tooltip: {
     style: {
-      fontSize: '19px',
-      padding: '10px'
-    }
+      fontSize: "19px",
+      padding: "10px",
+    },
   },
   plotOptions: {
     column: {
-        series: {
-          borderWidth: 0,
-          groupPadding: 0.1,
+      series: {
+        borderWidth: 0,
+        groupPadding: 0.1,
+      },
+      // pointPadding: 0.2,
+      dataLabels: {
+        enabled: true,
+        inside: false, // 레이블을 열 안쪽에 표시
+        color: "#333", // 레이블 색상 설정
+        style: {
+          fontSize: "15px",
+          fontWeight: "600",
+          textOutline: "none", // 텍스트 외곽선 제거
         },
-        // pointPadding: 0.2,
-        dataLabels: {
-          enabled: true,
-          inside: false, // 레이블을 열 안쪽에 표시
-          color: '#333', // 레이블 색상 설정
-          style: {
-              fontSize: '15px',
-              fontWeight: '600',
-              textOutline: 'none' // 텍스트 외곽선 제거
-          },
-          formatter: function() {
-            return Highcharts.numberFormat(this.y, 0, '', ','); // 데이터 값 포맷팅
-        }
-      }
+        formatter: function () {
+          return Highcharts.numberFormat(this.y, 0, "", ","); // 데이터 값 포맷팅
+        },
+      },
     },
-},
-legend: {
-  align: 'right',
-  verticalAlign: 'top',
-  layout: 'horizontal',
-  x: 0,
-  y: 0,
-  symbolRadius: 1,
-  symbolWidth: 18,
-  symbolHeight: 18,
-  symbol: function() {
-    return 'rect';
   },
-  itemStyle: {
-    fontSize: '16px'
+  legend: {
+    align: "right",
+    verticalAlign: "top",
+    layout: "horizontal",
+    x: 0,
+    y: 0,
+    symbolRadius: 1,
+    symbolWidth: 18,
+    symbolHeight: 18,
+    symbol: function () {
+      return "rect";
+    },
+    itemStyle: {
+      fontSize: "16px",
+    },
+    itemMarginTop: 0,
+    itemMarginBottom: 50,
+    backgroundColor:
+      (Highcharts.theme && Highcharts.theme.background2) || "white",
   },
-  itemMarginTop: 0,
-  itemMarginBottom: 50,
-  backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white'
-},
   series: [
-      {
-          name: '매출',
-          data: [20650, 20485, 22176],
-          color: '#f65742'
-      },
-      {
-          name: '매출총이익',
-          data: [1916, 1381, 970],
-          color: '#56423e'
-      },
-      {
-          name: '영업이익',
-          data: [1115, 559, 218],
-          color: '#bfb1a3',
-      },
-      {
-          name: '세전이익',
-          data: [1099, 602, 58],
-          color: '#00aacb'
-      },
-      {
-          name: '순이익',
-          data: [1495, 240, 66],
-          color: '#f0cc89'
-      },
-    ]
-  
+    {
+      name: "매출",
+      data: [20650, 20485, 22176],
+      color: "#f65742",
+    },
+    {
+      name: "매출총이익",
+      data: [1916, 1381, 970],
+      color: "#56423e",
+    },
+    {
+      name: "영업이익",
+      data: [1115, 559, 218],
+      color: "#bfb1a3",
+    },
+    {
+      name: "세전이익",
+      data: [1099, 602, 58],
+      color: "#00aacb",
+    },
+    {
+      name: "순이익",
+      data: [1495, 240, 66],
+      color: "#f0cc89",
+    },
+  ],
 });
