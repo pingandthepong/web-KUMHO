500eabab43ab5b6e4e8303bdc3a545fab5972786
pingandthepong <pingandthepong@gmail.com>
2024-04-11 16:20:37 +0900

    sub6_1 그누보드 게시판 추가

---
+++ gb/bbs/_head.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>회사소개-투자정보</title>
+  <link rel="shortcut icon" href="../favicon.ico">
+  <link rel="stylesheet" href="../../common/css/aos.css">
+  <link rel="stylesheet" href="../../common/css/common.css">
+  <link rel="stylesheet" href="../../sub1/common/css/sub1common.css">
+  <!-- 그누보드 게시판이 들어가므로 아래의 sub1_3은 필요없음 -->
+  <!-- <link rel="stylesheet" href="../../sub1/css/sub1_content3.css"> -->
+  <style>
+    #headerArea #gnb {
+      background: none;
+    }
+    #headerArea #gnb h3 .depth1 {
+      font-size: 1.36rem;
+      font-family: "Noto Sans KR", sans-serif;
+    }
+    #headerArea #gnb .dropdownmenu .menu ul a {
+      font-size: 1rem;
+    }
+    #headerArea .top_menu a {
+      font-size: 1rem;
+    }
+    #content {
+      font-family: "Noto Sans KR", sans-serif;
+      padding-bottom: 100px;
+    }
+    .sub_nav li {
+      width: calc(1400px / 4);
+      text-align: center;
+      position: relative;
+    }
+    #content .title_area .line_map {
+      font-size: 1rem;
+    }
+    #content .title_area h2 {
+      font-size: 3.75rem;
+      font-family: "Noto Sans KR", sans-serif;
+    }
+    #content .content_area {
+      width: 1400px;
+      margin: 0 auto;
+      font-size: 1.25rem;
+    }
+  </style>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+  <script
+    src="https://kit.fontawesome.com/5c48cfd0d4.js"
+    crossorigin="anonymous"></script>
+  <script src="../../common/js/prefixfree.min.js"></script>
+  
+</head>
+<body>
+  <!-- skipNav -->
+  <div id="skipNav">
+    <a href="#content">본문 바로가기</a>
+    <a href="#gnb">글로벌 내비게이션 바로가기</a>
+  </div>
+  <div id="wrap">
+    <!-- header -->
+    <header id="headerArea">
+      <span class="on"></span>
+      <!-- header .logo -->
+      <div class="headerInner">
+        <h1 class="logo"><a href="../../index.html" tabindex="0">금호건설</a></h1>
+
+        <!-- header #gnb -->
+        <nav id="gnb">
+          <h2 class="hidden">글로벌 네비게이션</h2>
+          <ul class="dropdownmenu">
+            <li class="m1 menu">
+              <h3><a class="depth1" href="../../sub1/sub1_1.html">회사소개</a></h3>
+              <ul>
+                <li><a href="../../sub1/sub1_1.html">VISION</a></li>
+                <li><a href="../../sub1/sub1_2.html">연혁</a></li>
+                <li><a href="../../sub1/sub1_3.html">투자정보</a></li>
+                <li><a href="../../sub1/sub1_4.html">협업시스템 다울</a></li>
+                <li><a href="../../sub1/sub1_5.html">오시는 길</a></li>
+              </ul>
+            </li>
+            <li class="m2 menu">
+              <h3><a class="depth1" href="../../sub2/sub2_1.html">사업분야</a></h3>
+              <ul>
+                <li><a href="../../sub2/sub2_1.html">건축사업</a></li>
+                <li><a href="../../sub2/sub2_2.html">토목사업</a></li>
+                <li><a href="../../sub2/sub2_3.html">주택사업</a></li>
+                <li><a href="../../sub2/sub2_4.html">플랜트/환경사업</a></li>
+                <li><a href="../../sub2/sub2_5.html">실적</a></li>
+              </ul>
+            </li>
+            <li class="m3 menu">
+              <h3><a class="depth1" href="../../sub3/sub3_1.html">기술혁신</a></h3>
+              <ul>
+                <li><a href="../../sub3/sub3_1.html">토목건축연구소</a></li>
+                <li><a href="../../sub3/sub3_2.html">환경연구소</a></li>
+                <li><a href="../../sub3/sub3_3.html">수상실적</a></li>
+              </ul>
+            </li>
+            <li class="m4 menu">
+              <h3><a class="depth1" href="../../sub4/sub4_1.html">지속가능경영</a></h3>
+              <ul>
+                <li><a href="../../sub4/sub4_1.html">윤리경영</a></li>
+                <li><a href="../../sub4/sub4_2.html">품질경영</a></li>
+                <li><a href="../../sub4/sub4_3.html">안전경영</a></li>
+                <li><a href="../../sub4/sub4_4.html">환경경영</a></li>
+              </ul>
+            </li>
+            <li class="m5 menu">
+              <h3><a class="depth1" href="../../sub5/sub5_1.html">인재채용</a></h3>
+              <ul>
+                <li><a href="../../sub5/sub5_1.html">인사제도</a></li>
+                <li><a href="../../sub5/sub5_2.html">직무가이드</a></li>
+                <li><a href="../../sub5/sub5_3.html">채용/면접Tip</a></li>
+                <li><a href="../../sub5/sub5_4.html">채용안내</a></li>
+              </ul>
+            </li>
+            <li class="m6 menu">
+              <h3><a class="depth1" href="../../sub6/sub6_1.html">홍보센터</a></h3>
+              <ul>
+                <li><a href="https://pingandthepong.mycafe24.com/gb/bbs/board.php?bo_table=free">금호뉴스</a></li>
+                <li><a href="../../sub6/sub6_2.html">분양뉴스</a></li>
+                <li><a href="../../sub6/sub6_3.html">고객센터</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+
+        <!-- header .top_menu -->
+        <ul class="top_menu">
+          <li class="signin"><a href="#">로그인</a></li>
+          <li class="signup"><a href="#">회원가입</a></li>
+        </ul>
+      </div>
+    </header>
+
+    <!-- .visual -->
+    <div class="visual">
+      <span>홍보센터</span>
+    </div>
+
+    <!-- .sub_nav -->
+    <div class="sub_nav">
+      <ul>
+        <li><a href="https://pingandthepong.mycafe24.com/gb/bbs/board.php?bo_table=free" class="current">금호뉴스</a></li>
+        <li><a href="../../sub6/sub6_2.html">분양뉴스</a></li>
+        <li><a href="../../sub6/sub6_3.html">고객센터</a></li>
+      </ul>
+    </div>
+
+    <!-- article -->
+    <article id="content">
+      <!-- .title_area -->
+      <div class="title_area">
+        <div class="line_map">
+          <span class="home">Home</span> &gt; 홍보센터 &gt;
+          <strong>금호뉴스</strong>
+        </div>
+        <h2>금호뉴스</h2>
+        <p>Think of the people and nature</p>
+      </div>
+
+      <!-- .content_area -->
+      <div class="content_area">
