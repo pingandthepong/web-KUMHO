ec4025b530e40fccdac305ac18e5dc2b53502fcf
pingandthepong <pingandthepong@gmail.com>
2024-04-12 12:48:48 +0900

    sub4_3 마크업 및 스타일링 수정

--- sub4/js/sub4_3_policyTab.js
+++ sub4/js/sub4_3_policyTab.js
@@ -15,6 +15,17 @@ $('.tab').on('click', function(e) {
   $(`.policy .contlist:eq(${ind})`).show();
 });
 
+// 스크롤 시 visual_img 너비 조정
+$(window).on('scroll', function() {
+  var wScroll = $(window).scrollTop();
+  var viOffsetTop = $('.visual_img').offset().top - 650;
+
+  if (wScroll >= viOffsetTop) {
+    $('.visual_img').addClass('on');
+  } else {
+    $('.visual_img').removeClass('on');
+  }
+});
 
 // img 클릭 시 .darkbg
 $('.openBtn').on('click', function(e) {
