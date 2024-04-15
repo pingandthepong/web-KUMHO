766d444eb34c844547e99fcb5ab764d518844672
pingandthepong <pingandthepong@gmail.com>
2024-04-15 21:38:02 +0900

    sub5_1 ul 중첩 오류 해결 - html() 대신 replaceWith() 사용 & .youtube_link 클릭 오류 해결 - .on('click', '.youtube_link', function(e){} 사용

--- sub5/js/youtube.js
+++ sub5/js/youtube.js
@@ -1,32 +1,34 @@
 $.ajax({
   url: "../data/youtube.json",
   dataType: "json",
-  success: function(data) {
+  success: function (data) {
     var useData = data.youtube;
 
     function dataPrint() {
-      var txt = "<ul>"
+      
+      var txt = `<ul>`;
 
       for (var i in useData) {
         txt += `<li>`,
-        txt += `<a href="#">`,
+        txt += `<a href="#" class="youtube_link">`,
         txt += `<div class="img_box">`,
-        txt += `<div class="img">`
+        txt += `<div class="img">`;
         txt += `<img src="${useData[i].imgSrc}" alt="${useData[i].imgAlt}">`,
         txt += `</div>`,
         txt += `</div>`,
         txt += `<div class="txt_box">`,
-        txt += `<p class="tit">${useData[i].tit}</p>`
-        txt += `<p class="txt">${useData[i].txt}</p>`
+        txt += `<p class="tit">${useData[i].tit}</p>`;
+        txt += `<p class="txt">${useData[i].txt}</p>`;
         txt += `</div>`,
         txt += `</a>`,
         txt += `</li>`
       }
-
-      txt += "</ul>"
-      $('.contlist ul').html(txt);
+      
+      txt += `</ul>`;
+      
+      $(".cont1 > ul").replaceWith(txt);
     }
     dataPrint();
-
-  }
+  },
 });
+
---
+++ sub5/js/youtubePopup.js
@@ -0,0 +1,4 @@
+$(document).on('click', '.youtube_link', function(e) {
+  e.preventDefault();
+  // 클릭 이벤트 처리 로직 추가
+});
