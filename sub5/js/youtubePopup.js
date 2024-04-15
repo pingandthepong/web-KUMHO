cbd8d6385fa0acd1e3654525a8896058adde5b75
pingandthepong <pingandthepong@gmail.com>
2024-04-16 08:41:04 +0900

    sub5_1 .youtube_link 클릭시 iframe 띄우고 modal_box 클릭시 src 비워주기까지 했으나, youtubePopup.js에서 iframe이 중복되어 모두 로드됨

--- sub5/js/youtubePopup.js
+++ sub5/js/youtubePopup.js
@@ -1,15 +1,45 @@
+$.ajax ({
+  url: "../data/youtubePopup.json",
+  dataType: "json",
+  success: function(data) {
+    var useData = data.youtubePopup;
+
+    let txt = ``;
+
+    function dataPrint() {
+
+      for (let i in useData) {
+        txt += `<iframe width="560" height="315" `,
+        txt += `src="${useData[i].iframeSrc}" `
+        txt += `title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>`,
+        txt += `</iframe>`
+      }
+      
+      $('.youtube_init').html(txt);
+    }
+    dataPrint();
+  }
+});
+
+
+
+// 동적으로 생성된 링크이기 때문에 이벤트 위임 사용
 $(document).on('click', '.youtube_link', function(e) {
   e.preventDefault();
-  
-  var ind = $(this).index('.youtube_link');
   
   $('.modal_box').fadeIn();
   $('.popup_youtube').fadeIn();
 });
 
 
-$('.modal_box').click(function() {
-
+$('.modal_box').click(function(e){
+  e.preventDefault();
+  
   $(this).fadeOut();
-  $('.popup_youtube').hide();
-})
+  $('.popup_youtube').fadeOut();
+  
+  let video = $('.youtube_init').find('iframe').attr("src");
+  $('.youtube_init').find('iframe').attr("src", "");
+  $('.youtube_init').find('iframe').attr("src", video);
+});
+
