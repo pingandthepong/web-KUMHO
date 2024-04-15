efd4cab4a6a28f4ef54826bdf3d016a1177778ec
pingandthepong <pingandthepong@gmail.com>
2024-04-15 15:15:14 +0900

    '환경관리' accordion 오류 수정

--- sub4/js/manageAccordion.js
+++ sub4/js/manageAccordion.js
@@ -1,25 +1,28 @@
-// 클릭 이벤트 핸들러
-var list = $('.manage_accordion .manage_list')
-
 $('.manage_accordion .trigger').on('click', function(e) {
   e.preventDefault();
-
+  
+  var list = $('.manage_list')
   var myList = $(this).parents('.manage_list');
 
 
   // 닫혀있다면
   if(myList.hasClass('hide')) {
     // 모두 다 닫고
-    list.removeClass('show').addClass('hide')
-    list.find('.a').slideUp(100);
+    list.find('.a').slideUp('fast');
+    list.removeClass('show').addClass('hide');
 
     // 클래스 바꾸기
-    myList.removeClass('hide').addClass('show').find('.a').slideDown(100);
+    myList.removeClass('hide').addClass('show');
+    myList.find('.a').slideDown('slow');
     
 
     // 열려있다면
   } else {
+
+    // 다른 list show 지우기
+    list.removeClass('show').addClass('hide');
     // 클래스 바꾸기
-    myList.removeClass('show').addClass('hide').find('.a').slideUp(100);
+    myList.removeClass('show').addClass('hide');
+    myList.find('.a').slideUp('fast');
   }
 })
