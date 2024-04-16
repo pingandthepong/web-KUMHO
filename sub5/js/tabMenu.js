25dc23fb6b4c894bb08fa0b7cee7d6b3c8143c6d
pingandthepong <pingandthepong@gmail.com>
2024-04-16 16:19:19 +0900

    sub5_1 직무소개 기본 마크업 및 스타일링

---
+++ sub5/js/tabMenu.js
@@ -0,0 +1,15 @@
+// 초기 상태
+$('.tab_menu:eq(1) .tab').addClass('current');
+$('.contlist:eq(1)').show();
+
+// 클릭 이벤트 핸들러
+$('.tab').on('click focus', function(e) {
+  e.preventDefault();
+  
+  $('.tab_menu .tab').removeClass('current');
+  $(this).addClass('current');
+  
+  let ind = $(this).index('.tab');
+  $('.contlist').hide();
+  $(`.contlist:eq(${ind})`).show();
+});
