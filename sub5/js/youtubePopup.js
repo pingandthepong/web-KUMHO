f399475ae341e0c6394ff11a23f909c336b55d4c
pingandthepong <pingandthepong@gmail.com>
2024-04-16 15:01:32 +0900

    sub5_1 youtubePopup 기능 추가

--- sub5/js/youtubePopup.js
+++ sub5/js/youtubePopup.js
@@ -1,45 +1,39 @@
-$.ajax ({
-  url: "../data/youtubePopup.json",
-  dataType: "json",
-  success: function(data) {
-    var useData = data.youtubePopup;
+$(document).ready(function() {
+  // 링크가 동적으로 생성되기 때문에 비디오 정보 미리 가져오기
+  $.ajax({
+    url: "../data/youtubePopup.json",
+    dataType: "json",
+    success: function(data) {
+      var useData = data.youtubePopup;
 
-    let txt = ``;
+      // 팝업 비디오 정보를 변수에 저장
+      var videos = useData.map(function(item) {
+        return `<iframe src="${item.iframeSrc}" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+      });
 
-    function dataPrint() {
-
-      for (let i in useData) {
-        txt += `<iframe width="560" height="315" `,
-        txt += `src="${useData[i].iframeSrc}" `
-        txt += `title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>`,
-        txt += `</iframe>`
-      }
-      
-      $('.youtube_init').html(txt);
+      // 클릭 이벤트 핸들러를 한 번만 바인딩
+      $('.youtube_link').click(function(e) {
+        e.preventDefault();
+        
+        $('.modal_box').fadeIn();
+        $('.popup_youtube').fadeIn();
+        
+        // 클릭된 링크의 인덱스를 가져와서 해당하는 비디오를 띄우기
+        var index = $(this).index('.youtube_link');
+        $(".youtube_init").html(videos[index]);
+      });
     }
-    dataPrint();
-  }
+  });
+
+  // 모달 박스 클릭 시 동작
+  $('.modal_box').click(function() {
+    $(this).fadeOut();
+    $('.popup_youtube').fadeOut();
+
+    // 비디오를 초기화
+    $('.youtube_init iframe').each(function() {
+      var videoSrc = $(this).attr("src");
+      $(this).attr("src", "").attr("src", videoSrc);
+    });
+  });
 });
-
-
-
-// 동적으로 생성된 링크이기 때문에 이벤트 위임 사용
-$(document).on('click', '.youtube_link', function(e) {
-  e.preventDefault();
-  
-  $('.modal_box').fadeIn();
-  $('.popup_youtube').fadeIn();
-});
-
-
-$('.modal_box').click(function(e){
-  e.preventDefault();
-  
-  $(this).fadeOut();
-  $('.popup_youtube').fadeOut();
-  
-  let video = $('.youtube_init').find('iframe').attr("src");
-  $('.youtube_init').find('iframe').attr("src", "");
-  $('.youtube_init').find('iframe').attr("src", video);
-});
-
