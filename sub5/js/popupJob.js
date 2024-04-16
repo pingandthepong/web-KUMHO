7670e450b645843da2df89fdfb2015b0a7f7ae76
pingandthepong <pingandthepong@gmail.com>
2024-04-16 18:13:50 +0900

    sub5_1 직무소개 팝업 데이터 연결 성공

--- sub5/js/popupJob.js
+++ sub5/js/popupJob.js
@@ -8,11 +8,12 @@ xhr.onload = function() {
   //서버로 부터 전달된 json 데이터를 responseText 속성을 통해 가져올 수 있다.
   // parse() 메소드를 호출하여 자바스크립트 객체로 변환한다.
   responseObject = JSON.parse(xhr.responseText);
+  // console.log(responseObject); // 확인
 
 };
 
 // 요청 준비 및 전송
-xhr.open('GET', '../../data/popupJob.json', true);
+xhr.open('GET', '../data/popupJob.json', true);
 xhr.send(null);
 
 
@@ -25,14 +26,15 @@ $(document).ready(function(){
   $('.job_info_list a').click(function(e){
       e.preventDefault();
 
-      // 클릭 시 인덱스 번호만 빼내면 끝남!
+      // 클릭 시 인덱스 번호 추출
       var ind = $(this).index('.job_info_list a');
+
+      // console.log('클릭 이벤트 발생'); // 확인
 
       $('.modal_box2').fadeIn('fast');
       $('.popup_job').fadeIn('slow');
 
       newContent=''; // 이전 데이터가 비워지도록 초기화
-      newContent+=`<div class="popup_job modal2">`;
       newContent+=`<h5>${responseObject.popupJob[ind].title}<span>${responseObject.popupJob[ind].category}</span></h5>`;
       newContent+=`<dl class="introduce">`;
       newContent+=`<dt>직무특성 및 소개</dt>`;
@@ -50,16 +52,12 @@ $(document).ready(function(){
       newContent+=`<dt>Career Path 및 발전 방향</dt>`;
       newContent+=`<dd>${responseObject.popupJob[ind].direction}</dd>`;
       newContent+=`</dl>`;
-      newContent+=`<div class="popupjob_close">`;
-      newContent+=`<a href="#">닫기</a>`;
-      newContent+=`</div>`;
-      newContent+=`</div>`;
 
 
 
 
-      $('.popup_job').html(newContent);
-      // console.log(responseObject);
+      $('.popup_job_wrap').html(newContent);
+  
   });
  
 
