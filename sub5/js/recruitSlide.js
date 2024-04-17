6940df14ab39a388af8fa5752da6a84d63cab776
pingandthepong <pingandthepong@gmail.com>
2024-04-17 21:07:08 +0900

    sub5_2 section3 .progress 버튼 이벤트 핸들러 설정

--- sub5/js/recruitSlide.js
+++ sub5/js/recruitSlide.js
@@ -1,1 +1,30 @@
-// ToDo: 
+// 초기 상태
+let cnt = 1;
+let slideTotal = $('.swiper-slide').length;
+
+$('.num').text(cnt);
+$('.max').text(slideTotal);
+
+
+// 버튼 이벤트 핸들러
+$('.slide_btn').click(function(e) {
+  e.preventDefault();
+
+
+  if ($(this).is($('#next'))) {
+    // next를 누르면
+    cnt++;
+    if(cnt > slideTotal) {
+      cnt = 1;
+    }
+
+  } else if($(this).is($('#prev'))) {
+    cnt--;
+    if(cnt == 0) {
+      cnt = slideTotal;
+    }
+  }
+
+  $('.num').text(cnt);
+
+})
