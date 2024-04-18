5d1c2fce01f529d3b007d402e394394885be0860
pingandthepong <pingandthepong@gmail.com>
2024-04-18 15:26:24 +0900

    sub5_3 경력채용 section 마크업 및 스타일링

--- sub5/js/circle.js
+++ sub5/js/circle.js
@@ -106,8 +106,154 @@ var progressBar4 = new ProgressBar.Circle('#progressBar4', {
   }
 });
 
+
+var progressBar5 = new ProgressBar.Circle('#progressBar5', {
+  strokeWidth: 8, // 선 두께
+  easing: 'easeInOut', // 애니메이션 이징
+  duration: 1400, // 애니메이션 지속 시간 (밀리초)
+  color: '#ef0010', // 진행률 바 색상
+  trailColor: '#fff', // 진행률 바 배경 색상
+  trailWidth: 8, // 배경 선 두께
+  svgStyle: {width: '100%', height: '100%'}, // SVG 스타일
+  text: {
+    style: {
+      // 텍스트 스타일
+      color: '#007bff',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false // 텍스트 자동 스타일링 비활성화
+  },
+  step: function(state, bar) {
+    // 진행률 변화에 따른 콜백 함수
+    bar.setText(Math.round(bar.value() * 100) + ' %'); // 퍼센트 표시
+  }
+});
+
+var progressBar6 = new ProgressBar.Circle('#progressBar6', {
+  strokeWidth: 8, // 선 두께
+  easing: 'easeInOut', // 애니메이션 이징
+  duration: 1400, // 애니메이션 지속 시간 (밀리초)
+  color: '#ef0010', // 진행률 바 색상
+  trailColor: '#fff', // 진행률 바 배경 색상
+  trailWidth: 8, // 배경 선 두께
+  svgStyle: {width: '100%', height: '100%'}, // SVG 스타일
+  text: {
+    style: {
+      // 텍스트 스타일
+      color: '#007bff',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false // 텍스트 자동 스타일링 비활성화
+  },
+  step: function(state, bar) {
+    // 진행률 변화에 따른 콜백 함수
+    bar.setText(Math.round(bar.value() * 100) + ' %'); // 퍼센트 표시
+  }
+});
+
+var progressBar7 = new ProgressBar.Circle('#progressBar7', {
+  strokeWidth: 8, // 선 두께
+  easing: 'easeInOut', // 애니메이션 이징
+  duration: 1400, // 애니메이션 지속 시간 (밀리초)
+  color: '#ef0010', // 진행률 바 색상
+  trailColor: '#fff', // 진행률 바 배경 색상
+  trailWidth: 8, // 배경 선 두께
+  svgStyle: {width: '100%', height: '100%'}, // SVG 스타일
+  text: {
+    style: {
+      // 텍스트 스타일
+      color: '#007bff',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false // 텍스트 자동 스타일링 비활성화
+  },
+  step: function(state, bar) {
+    // 진행률 변화에 따른 콜백 함수
+    bar.setText(Math.round(bar.value() * 100) + ' %'); // 퍼센트 표시
+  }
+});
+
+var progressBar8 = new ProgressBar.Circle('#progressBar8', {
+  strokeWidth: 8, // 선 두께
+  easing: 'easeInOut', // 애니메이션 이징
+  duration: 1400, // 애니메이션 지속 시간 (밀리초)
+  color: '#ef0010', // 진행률 바 색상
+  trailColor: '#fff', // 진행률 바 배경 색상
+  trailWidth: 8, // 배경 선 두께
+  svgStyle: {width: '100%', height: '100%'}, // SVG 스타일
+  text: {
+    style: {
+      // 텍스트 스타일
+      color: '#007bff',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false // 텍스트 자동 스타일링 비활성화
+  },
+  step: function(state, bar) {
+    // 진행률 변화에 따른 콜백 함수
+    bar.setText(Math.round(bar.value() * 100) + ' %'); // 퍼센트 표시
+  }
+});
+
+var progressBar9 = new ProgressBar.Circle('#progressBar9', {
+  strokeWidth: 8, // 선 두께
+  easing: 'easeInOut', // 애니메이션 이징
+  duration: 1400, // 애니메이션 지속 시간 (밀리초)
+  color: '#ef0010', // 진행률 바 색상
+  trailColor: '#fff', // 진행률 바 배경 색상
+  trailWidth: 8, // 배경 선 두께
+  svgStyle: {width: '100%', height: '100%'}, // SVG 스타일
+  text: {
+    style: {
+      // 텍스트 스타일
+      color: '#007bff',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false // 텍스트 자동 스타일링 비활성화
+  },
+  step: function(state, bar) {
+    // 진행률 변화에 따른 콜백 함수
+    bar.setText(Math.round(bar.value() * 100) + ' %'); // 퍼센트 표시
+  }
+});
+
+
+
+
+
+
 // ProgressBar 시작
 progressBar1.animate(0.25, 0); // 0부터 1까지 애니메이션 실행
-progressBar2.animate(0.5, 0); // 0부터 1까지 애니메이션 실행
-progressBar3.animate(0.75, 0); // 0부터 1까지 애니메이션 실행
-progressBar4.animate(1, 0); // 0부터 1까지 애니메이션 실행
+progressBar2.animate(0.5, 0); 
+progressBar3.animate(0.75, 0); 
+progressBar4.animate(1, 0);
+progressBar5.animate(0.2, 0);
+progressBar6.animate(0.4, 0);
+progressBar7.animate(0.6, 0);
+progressBar8.animate(0.8, 0);
+progressBar9.animate(1, 0);
