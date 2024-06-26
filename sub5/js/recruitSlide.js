79883c3a6745428b862b0d0470774ad671a4bc82
pingandthepong <pingandthepong@gmail.com>
2024-06-27 03:33:18 +0900

    sub5_2 드래그 시에도 이미지 number 반영되도록 #64

--- sub5/js/recruitSlide.js
+++ sub5/js/recruitSlide.js
@@ -1,30 +1,51 @@
 // 초기 상태
 let cnt = 1;
-let slideTotal = $('.swiper-slide').length;
+let slideTotal = $(".swiper-slide").length;
 
-$('.num').text(cnt);
-$('.max').text(slideTotal);
+$(".num").text(cnt);
+$(".max").text(slideTotal);
 
+// function slideTotalChange(target) {
+//   if ($(target).is($("#next"))) {
+//     // next를 누르면
+//     cnt++;
+//     if (cnt > slideTotal) {
+//       cnt = 1;
+//     }
+//   } else if ($(target).is($("#prev"))) {
+//     cnt--;
+//     if (cnt == 0) {
+//       cnt = slideTotal;
+//     }
+//   }
 
-// 버튼 이벤트 핸들러
-$('.slide_btn').click(function(e) {
-  e.preventDefault();
+//   $(".num").text(cnt);
+// }
 
+// 버튼 이벤트 핸들러 (아래의 방법으로 하니 버튼 이벤트 필요 없어짐)
+// $(".slide_btn").on("click", function (e) {
+//   e.preventDefault();
+//   slideTotalChange(this);
+//   $(".swiper-slide").index(this).addClass("on");
+// });
 
-  if ($(this).is($('#next'))) {
-    // next를 누르면
-    cnt++;
-    if(cnt > slideTotal) {
-      cnt = 1;
-    }
-
-  } else if($(this).is($('#prev'))) {
-    cnt--;
-    if(cnt == 0) {
-      cnt = slideTotal;
-    }
+// 방법1) swiper-slide의 aria-label 속성을 가져와 .num, .max에 담기 (안담아짐.. 일단 담아보자.. 성공!)
+$(document).ready(function () {
+  function updateAriaNum() {
+    let ariaNum = $(".swiper-slide")
+      .map(function () {
+        if ($(this).hasClass("swiper-slide-active")) {
+          return $(this).attr("aria-label").split(" / ")[0];
+        }
+      })
+      .get();
+    // console.log(ariaNum); // ['1']
+    $(".num").text(ariaNum);
   }
 
-  $('.num').text(cnt);
+  const observer = new MutationObserver(updateAriaNum);
 
-})
+  $(".swiper-slide").each(function () {
+    observer.observe(this, { attributes: true, attributeFilter: ["class"] });
+  });
+});
